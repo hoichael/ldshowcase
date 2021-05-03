d23439f3e2aaf449f2ca9ddebe552d6fe993497e
hoichael <31223078+hoichael@users.noreply.github.com>
2021-05-03 16:57:54 +0200

    Installed axios + setup fetching data from itch

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,19 @@
 import Head from 'next/head'
-
+import axios, { AxiosResponse } from "axios"
 
 export default function Home() {
+
+  function handleClick(): void {
+    axios.get('/api/get').then(function(res): void{
+      handleResponse(res);
+    });
+  }
+
+  function handleResponse(res: AxiosResponse):void {
+    console.log(res);
+    console.log(res.data.data.games)
+  }
+
   return (
     <div>
       <Head>
@@ -10,6 +22,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+      <button onClick={() => handleClick()}>FETCH</button>
 
     </div>
   )
