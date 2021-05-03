4da0165e2f6405fc3faea2a04f06947b1323a193
hoichael <31223078+hoichael@users.noreply.github.com>
2021-05-03 18:52:49 +0200

    Additions to GameCard structure and markup

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -21,6 +21,11 @@ const GameCard: React.FC<Props> = ( { info } ) => {
     return (
         <div className={styles.container} onClick={(e) => {changeHeight(e)}}>
             <img className={styles.thumbnail} src="https://img.itch.zone/aW1nLzI1Mjc0NTkucG5n/315x250%23c/btzn8z.png"></img>
+            <div className={styles.title}>TITLEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE</div>
+            <div className={styles.links}>
+                <div className={styles.link}><a href="https://noahro.itch.io/stabman">Itch Page</a></div>
+                <div className={styles.link}><a href="https://noahro.itch.io/stabman">LD Page</a></div>
+            </div>
         </div>
     )
 }
