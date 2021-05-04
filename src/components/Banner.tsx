9ca3523f0e44fdaab3a2414106ebbf5b02cc1d15
hoichael <31223078+hoichael@users.noreply.github.com>
2021-05-04 14:05:56 +0200

    Basic implementation of responsive banner text

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,13 +1,44 @@
+import { useState, useEffect } from "react"
 import styles from "/styles/Banner.module.css"
 
 const Banner: React.FC = () => {
 
+    const [text, setText] = useState("");
     const yearZero = "2019"
     const yearCurrent = new Date().getFullYear();
 
+    useEffect(() => {
+        adjustText();
+        window.onresize = () => {
+            adjustText();
+        };
+    }, [])
+
+    function adjustText(): any {
+        let width = window.innerWidth;
+        switch(true) {
+            case width >= 1400: {
+                setText("biggest")
+                break;
+            }
+            case width >=1000: {
+                setText("bigger")
+                break;
+            }
+            case width >= 800: {
+                setText("big")
+                break;
+            }
+            default: {
+                setText("smol")
+                break;
+            }
+        }
+    }
+
     return (
         <div className={styles.container}>
-            <div className={styles.title}>Ludum Dare Games {yearZero} - {yearCurrent}</div>
+            <div className={styles.title}>{text}</div>
         </div>
     )
 }
