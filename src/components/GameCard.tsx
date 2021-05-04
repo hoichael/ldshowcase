391337fc5b61b920f4a45621933eeead8624b879
hoichael <31223078+hoichael@users.noreply.github.com>
2021-05-04 17:43:29 +0200

    provisional mobile styling + implemented GameCard hover animation
    the latter is really bad. should rewrite at some point.

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -7,19 +7,39 @@ interface Props {
 
 const GameCard: React.FC<Props> = ( { info } ) => {
     let expanded = useRef(false);
+    let element:any = useRef();
+    let expandBool = useRef(false);
 
     function changeHeight(e:any): void {
+
+        // following 3 functions: really bad. will rewrite at some point. (probably not)
+        if(expandBool.current) { return }
+        element.current = e.currentTarget;
         if(expanded.current) {
             e.currentTarget.classList.remove(styles.expanded)
+            delayedClassChange();
             expanded.current = false;
         } else {
             e.currentTarget.classList.add(styles.expanded)
+            e.currentTarget.classList.remove(styles.hover)
             expanded.current = true;
         }
     }
 
+    function delayedClassChange():void {
+        if(expandBool.current) { return }
+        expandBool.current = true;
+        element.current.addEventListener('transitionend', changeClass);
+    }
+
+    function changeClass():void {
+        element.current.classList.add(styles.hover)
+        expandBool.current = false;
+        element.current.removeEventListener("transitionend", changeClass)
+    }
+
     return (
-        <div className={styles.container} onClick={(e) => {changeHeight(e)}}>
+        <div className={styles["container"] + " " + styles["hover"]} onClick={(e) => {changeHeight(e)}}>
             <img className={styles.thumbnail} src="https://img.itch.zone/aW1nLzI1Mjc0NTkucG5n/315x250%23c/btzn8z.png"></img>
             <div className={styles.text}>
                 TITLEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE
